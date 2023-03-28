321fab62cbacd923dc33f9a277ae99618ff936cd
MarkXtenda <mark.i2p.1998@gmail.com>
2023-03-28 14:37:20 +1100

    1. Front end with basic functionality added. Basic CSS added.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import Home from './components/Home';
+import Login from './components/Login';
+import Notes from './components/Notes';
+import ShoppingCart from './components/ShoppingCart';
+import Footer from './components/Footer';
 import './App.css';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(true);
+  const [cart, setCart] = useState<any[]>([]);
+
+  const handleLogin = () => {
+    setLoggedIn(true);
+  };
+
+  const handleLogout = () => {
+    setLoggedIn(false);
+  };
+
+  const addToCart = (note: string) => {
+    // Possibly can fit alert that user cant add more than one note to the cart
+    (cart.some((item) => item === note)) ? cart : setCart([...cart, note])
+  };
+
+  const removeFromCart = (note: string) => {
+    setCart(cart.filter((item) => item !== note));
+  };
+
+  //   return (
+  //     <div>
+  //       <Header loggedIn={loggedIn} handleLogout={handleLogout}/>
+  //       <nav>
+  //         <ul>
+  //           <li>
+  //             <a href="#">Home</a>
+  //           </li>
+  //           <li>
+  //             <a href="#">Notes</a>
+  //           </li>
+  //           {loggedIn ? (
+  //             <li>
+  //               <button onClick={handleLogout}>Logout</button>
+  //             </li>
+  //           ) : (
+  //             <li>
+  //               <a href="#">Login</a>
+  //             </li>
+  //           )}
+  //           <li>
+  //             <a href="#">Cart ({cart.length})</a>
+  //           </li>
+  //         </ul>
+  //       </nav>
+  //       {loggedIn ? (
+  //         <Notes addToCart={addToCart} />
+  //       ) : (
+  //         <Login handleLogin={handleLogin} />
+  //       )}
+  //       <ShoppingCart cart={cart} removeFromCart={removeFromCart} />
+  //       <Footer />
+  //     </div>
+  //   );
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header cartLength={cart.length} loggedIn={loggedIn} handleLogout={handleLogout} />
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        <Route path="/login" element={<Login handleLogin={handleLogin} />}></Route>
+        <Route path="/notes" element={<Notes addToCart={addToCart} />}></Route>
+        <Route path="/cart" element={<ShoppingCart cart={cart} removeFromCart={removeFromCart} />}></Route>
+      </Routes>
+      <Footer />
+    </Router>
   );
 }
 
